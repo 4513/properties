e2c7867cd0246edebcfe818eeb6cba8262b7db70
MiBo <michal.boris@feo.cz>
2023-07-23 00:29:51 +0200

    WIP: PerUnit

---
+++ src/Units/PerUnit/PerNotSpecified.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace MiBo\Properties\Units\PerUnit;
+
+/**
+ * Class PerNotSpecified
+ *
+ * @package MiBo\Properties\Units\PerUnit
+ *
+ * @author Michal Boris <michal.boris27@gmail.com>
+ *
+ * @since x.x
+ *
+ * @no-named-arguments Parameter names are not covered by the backward compatibility promise.
+ */
+class PerNotSpecified
+{
+
+}
